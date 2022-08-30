02fb1ab5e5df1fb3ebcd0da411e0d7ab361ee93b
Dmytro Kurovskyi <dmytro.kurovskyi@gmail.com>
2022-08-30 16:09:38 +0300

    refactor: refactored trim decorator
    Co-authored-by: Kathryn Verkhohliad <99083233+kathalie@users.noreply.github.com>

--- src/common/decorators/trim.decorator.ts
+++ src/common/decorators/trim.decorator.ts
@@ -4,11 +4,12 @@ import { isArray, map, trim } from 'lodash';
 export const Trim = (): PropertyDecorator => {
   return Transform((params) => {
     const value = params.value as string[] | string;
+    const multipleSpaceCharacters = /\s\s+/g;
 
     if (isArray(value)) {
-      return map(value, (v) => trim(v).replace(/\s\s+/g, ' '));
+      return map(value, (v) => trim(v).replace(multipleSpaceCharacters, ' '));
     }
 
-    return trim(value).replace(/\s\s+/g, ' ');
+    return trim(value).replace(multipleSpaceCharacters, ' ');
   });
 };
